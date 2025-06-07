c06ce450e5d96ab71b41dd332158ec366bdaebef
Bùi Trung Hiếu <trunghieu.bui201@gmail.com>
2025-06-07 10:27:40 +0700

    LL-01: Add router to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:latern_link/src/rust/api/simple.dart';
+import 'package:latern_link/config/app_config.dart';
+import 'package:latern_link/config/app_routes.dart';
+import 'package:latern_link/config/themes.dart';
+import 'package:latern_link/di/global/get_it_initializer.dart';
 import 'package:latern_link/src/rust/frb_generated.dart';
+import 'package:loggy/loggy.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  GetItInitializer().setUp();
+
   await RustLib.init();
+
+  Loggy.initLoggy(logPrinter: const PrettyPrinter(showColors: true));
+
   runApp(const LaternLinkApp());
 }
 
@@ -12,15 +22,14 @@ class LaternLinkApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('flutter_rust_bridge quickstart')),
-        body: Center(
-          child: Text(
-            'Action: Call Rust `greet("Tom")`\nResult: `${greet(name: "Tom")}`',
-          ),
-        ),
-      ),
+    return MaterialApp.router(
+      routerConfig: AppRoutes.router,
+      title: AppConfig.title,
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      builder: (context, child) {
+        return Scaffold(body: child);
+      },
     );
   }
 }
